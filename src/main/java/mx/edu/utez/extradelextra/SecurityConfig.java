2bc5fbf2b7ca9c1af9d0c0bbfede361189cb572e
Rebecca C <20203tn077@utez.edu.mx>
2023-04-26 01:37:56 -0600

    Finalización de práctica con mis amigas

--- src/main/java/mx/edu/utez/extradelextra/SecurityConfig.java
+++ src/main/java/mx/edu/utez/extradelextra/SecurityConfig.java
@@ -1,18 +1,31 @@
 package mx.edu.utez.extradelextra;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mx.edu.utez.extradelextra.model.Login;
+import mx.edu.utez.extradelextra.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private LoginService loginService;
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withDefaultPasswordEncoder()
@@ -36,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/", "/assets/**").permitAll()
+                .requestMatchers("/", "/assets/**", "/bitacora").permitAll()
                 .requestMatchers("/capturistas").hasAnyRole("ADMINISTRATOR")
                 .requestMatchers("/clientes").hasAnyRole("ADMINISTRATOR", "TRANSCRIBER")
                 .requestMatchers("/pedidos").hasAnyRole("ADMINISTRATOR", "TRANSCRIBER", "CLIENT")
@@ -44,7 +57,15 @@ public class SecurityConfig {
                 .requestMatchers("/registro").hasAnyRole("ANONYMOUS")
                 .anyRequest().authenticated();
         http.formLogin()
-                .permitAll();
+                .permitAll()
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        Login login = new Login(authentication.getName(), request.getRemoteAddr());
+                        loginService.save(login);
+                        response.sendRedirect("/");
+                    }
+                });
         return http.build();
     }
 }
--- src/main/java/mx/edu/utez/extradelextra/controller/MainController.java
+++ src/main/java/mx/edu/utez/extradelextra/controller/MainController.java
@@ -1,10 +1,22 @@
 package mx.edu.utez.extradelextra.controller;
 
+import jakarta.validation.Valid;
+import mx.edu.utez.extradelextra.model.User;
+import mx.edu.utez.extradelextra.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
+    @Autowired
+    LoginService loginService;
+
     @GetMapping("")
     public String index() {
         return "index";
@@ -30,8 +42,26 @@ public class MainController {
         return "product_list";
     }
 
+    @GetMapping("bitacora")
+    public String logins(Model model) {
+        model.addAttribute("logins", loginService.findAll());
+        return "login_list";
+    }
+
     @GetMapping("registro")
-    public String register() {
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
         return "register_form";
+    }
+
+    @PostMapping("registro")
+    public String register(@Valid User user, BindingResult result, Model model, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            model.addAttribute("errorMessage", "Errores en los datos ingresados");
+            return "register_form";
+        }
+        attributes.addFlashAttribute("successMessage", "Usuario validado");
+        return "redirect:/";
     }
 }
